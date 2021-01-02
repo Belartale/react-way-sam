f31e2c0e00545ff6f71c456e95a9509ee201de32
belartale <belartale@gmail.com>
2021-01-02 21:56:06 +0200

    mod giving data for state

--- app-01/src/components/profile/MyPosts/MyPosts.jsx
+++ app-01/src/components/profile/MyPosts/MyPosts.jsx
@@ -1,43 +1,51 @@
 import React from "react";
 import Post from "./post/Post";
 
+let addTextActionCreator = (params) => {
+  return { type: "ADD-POST" };
+};
+
+let updataTextActionCreator = (text) => {
+  return { type: "UPDATE-POST", textPost: text };
+};
+
 const MyPosts = (props) => {
-	let newPostElement = React.createRef();
+  let newPostElement = React.createRef();
 
-	let addPost = (e) => {
-		e.preventDefault();
-		// props.addPost();
-		props.dispatch({ type: "ADD-POST" });
-	};
+  let addPost = (e) => {
+    e.preventDefault();
+    // props.addPost();
+    props.dispatch(addTextActionCreator());
+  };
 
-	let onChange = () => {
-		let text = newPostElement.current.value;
-		// props.updateNewPostText(text);
-		props.dispatch({ type: "UPDATE-POST", textPost: text });
-	};
+  let onChange = () => {
+    let text = newPostElement.current.value;
+    // props.updateNewPostText(text);
+    props.dispatch(updataTextActionCreator(text));
+  };
 
-	return (
-		<div className="block">
-			<form className="block" action="">
-				<textarea
-					className="control"
-					name=""
-					id=""
-					cols="30"
-					rows="5"
-					value={props.profileData.newPostText}
-					ref={newPostElement}
-					onChange={onChange}
-				/>
-				<button className="control" onClick={addPost}>
-					<p className="caption--size_3">Отправить</p>
-				</button>
-			</form>
-			<div>
-				<Post postData={props.profileData.posts} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="block">
+      <form className="block" action="">
+        <textarea
+          className="control"
+          value={props.profileData.newPostText}
+          ref={newPostElement}
+          onChange={onChange}
+          name=""
+          id=""
+          cols="30"
+          rows="5"
+        />
+        <button className="control" onClick={addPost}>
+          <p className="caption--size_3">Отправить</p>
+        </button>
+      </form>
+      <div>
+        <Post postData={props.profileData.posts} />
+      </div>
+    </div>
+  );
 };
 
 export default MyPosts;
--- app-01/src/redux/state.js
+++ app-01/src/redux/state.js
@@ -1,105 +1,90 @@
 export let store = {
-	_state: {
-		profileData: {
-			posts: [
-				{ id: 1, likes: 12, message: "Lorem, ipsum dolor. I have 12 likes" },
-				{
-					id: 2,
-					likes: 33,
-					message: "Lorem ipsum dolor sit amet. And I have 33 likes too",
-				},
-			],
-			newPostText: "something text of new post",
-		},
+  _state: {
+    profileData: {
+      posts: [
+        { id: 1, likes: 12, message: "Lorem, ipsum dolor. I have 12 likes" },
+        {
+          id: 2,
+          likes: 33,
+          message: "Lorem ipsum dolor sit amet. And I have 33 likes too",
+        },
+      ],
+      newPostText: "something text of new post",
+    },
 
-		dialogsPage: {
-			dialogsData: [
-				{ id: 1, name: "Artur" },
-				{ id: 2, name: "lorem" },
-			],
+    dialogsPage: {
+      dialogsData: [
+        { id: 1, name: "Artur" },
+        { id: 2, name: "lorem" },
+      ],
 
-			messageData: [
-				{ id: 1, message: "Lorem, ipsum dolor111." },
-				{ id: 2, message: "Lorem ipsum dolor sit amet." },
-			],
-			newMessageText: "something text of new message",
-		},
+      messageData: [
+        { id: 1, message: "Lorem, ipsum dolor111." },
+        { id: 2, message: "Lorem ipsum dolor sit amet." },
+      ],
+      newMessageText: "something text of new message",
+    },
 
-		sideBar: {
-			friends: [
-				"Saha",
-				"Andre",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lorem",
-				"Lili",
-			],
-		},
-	},
-	getState() {
-		return this._state;
-	},
+    sideBar: {
+      friends: [
+        "Saha",
+        "Andre",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lorem",
+        "Lili",
+      ],
+    },
+  },
+  getState() {
+    return this._state;
+  },
 
-	_callSubscribe() {
-		console.log("renderApp!!!!!!!!!!!!!!!!!!!!!!!!");
-	},
-	subscribe(observer) {
-		this._callSubscribe = observer;
-	},
+  _callSubscribe() {
+    console.log("renderApp!!!!!!!!!!!!!!!!!!!!!!!!");
+  },
+  subscribe(observer) {
+    this._callSubscribe = observer;
+  },
 
-	// addPost() {
-	// 	let newPost = {
-	// 		id: 1,
-	// 		likes: 0,
-	// 		message: this._state.profileData.newPostText,
-	// 	};
-	// 	this._state.profileData.posts.push(newPost);
-	// 	this._state.profileData.newPostText = "";
-	// 	this._callSubscribe(this._state);
-	// },
-	// updateNewPostText(newText) {
-	// 	this._state.profileData.newPostText = newText;
-	// 	this._callSubscribe(this._state);
-	// },
+  addMessage() {
+    let newMessage = {
+      id: 3,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messageData.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._callSubscribe(this._state);
+  },
+  updateNewMessageText(newText) {
+    this._state.dialogsPage.newMessageText = newText;
+    this._callSubscribe(this._state);
+  },
 
-	addMessage() {
-		let newMessage = {
-			id: 3,
-			message: this._state.dialogsPage.newMessageText,
-		};
-		this._state.dialogsPage.messageData.push(newMessage);
-		this._state.dialogsPage.newMessageText = "";
-		this._callSubscribe(this._state);
-	},
-	updateNewMessageText(newText) {
-		this._state.dialogsPage.newMessageText = newText;
-		this._callSubscribe(this._state);
-	},
-
-	dispatch(action) {
-		if (action.type === "ADD-POST") {
-			let newPost = {
-				id: 1,
-				likes: 0,
-				message: this._state.profileData.newPostText,
-			};
-			this._state.profileData.posts.push(newPost);
-			this._state.profileData.newPostText = "";
-			this._callSubscribe(this._state);
-		} else if (action.type === "UPDATE-POST") {
-			this._state.profileData.newPostText = action.textPost;
-			this._callSubscribe(this._state);
-		}
-	},
+  dispatch(action) {
+    if (action.type === "ADD-POST") {
+      let newPost = {
+        id: 1,
+        likes: 0,
+        message: this._state.profileData.newPostText,
+      };
+      this._state.profileData.posts.push(newPost);
+      this._state.profileData.newPostText = "";
+      this._callSubscribe(this._state);
+    } else if (action.type === "UPDATE-POST") {
+      this._state.profileData.newPostText = action.textPost;
+      this._callSubscribe(this._state);
+    }
+  },
 };
 
 export default store;
