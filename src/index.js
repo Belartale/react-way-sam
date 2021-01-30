12ac41d107bdb25ad82327919111374544663ab3
Arthur Belan <belartale@gmail.com>
2021-01-31 01:06:48 +0200

    instal redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import store from "./redux/store";
+import store from "./redux/reduxStore";
 import "./index.css";
 
 let renderApp = (props) => {
