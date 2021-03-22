d50cd22299fde64a84bc8f987c98c1d3921dc1dc
belartale <belartale@gmail.com>
2021-03-22 18:58:27 +0200

    save

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { Button } from "@material-ui/core";
 const News = () => {
-	return (
-		<div>
-			<p>News</p>
-		</div>
-	);
+  return (
+    <div>
+      <p>News</p>
+      <Button variant="contained" color="primary">
+        some btn
+      </Button>
+    </div>
+  );
 };
 
 export default News;
